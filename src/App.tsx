34196e162da95847f8e5acd9f145c7b34b703b62
okuda1506 <okuda1506@gmail.com>
2023-08-12 13:16:30 +0900

    メモのフィルタリング機能

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ type Memo = {
   removed: boolean;
 };
 
+type Filter = "memo" | "archived" | "removed";
+
 export const App = () => {
   const [text, setText] = useState("");
   const [memos, setMemos] = useState<Memo[]>([]);
+  const [filter, setFilter] = useState<Filter>("memo");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
@@ -59,28 +62,53 @@ export const App = () => {
     setMemos((memos) => {
       const newMemos = memos.map((memo) => {
         if (memo.id === id) {
-          return {...memo, removed};
+          return { ...memo, removed };
         }
         return memo;
       });
 
       return newMemos;
     });
-  }
+  };
+
+  const handleSort = (filter: Filter) => {
+    setFilter(filter);
+  };
+
+  const filteredMemos = memos.filter((memo) => {
+    switch (filter) {
+      case 'memo':
+        return !memo.archived && !memo.removed;
+      case 'archived':
+        return memo.archived && !memo.removed;
+      case "removed":
+        return memo.removed;
+      default:
+        return memo;
+    }
+  });
 
   return (
     <div>
+      <select
+        defaultValue="all"
+        onChange={(e) => handleSort(e.target.value as Filter)}
+      >
+        <option value="memo">メモ</option>
+        <option value="archived">アーカイブ</option>
+        <option value="removed">ゴミ箱</option>
+      </select>
       <form
         onSubmit={(e) => {
           e.preventDefault();
           handleSubmit();
         }}
       >
-        <input type="text" value={text} onChange={(e) => handleChange(e)} />
-        <input type="submit" value="追加" onSubmit={handleSubmit} />
+        <input type="text" value={text} disabled={filter === 'archived' || filter === 'removed'} onChange={(e) => handleChange(e)} />
+        <input type="submit" value="追加" disabled={filter === 'archived' || filter === 'removed'} onSubmit={handleSubmit} />
       </form>
       <ul>
-        {memos.map((memo) => {
+        {filteredMemos.map((memo) => {
           return (
             <li key={memo.id}>
               <input
@@ -96,7 +124,7 @@ export const App = () => {
                 onChange={(e) => handleEdit(memo.id, e.target.value)}
               />
               <button onClick={() => handleRemove(memo.id, !memo.removed)}>
-                {memo.removed ? '復元' : '削除'}
+                {memo.removed ? "復元" : "削除"}
               </button>
             </li>
           );
